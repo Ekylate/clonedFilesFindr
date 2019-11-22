e89a3506175a381008cac9b55acb201f4d2925fa
ANTOINE Jean Timothee <jean-timothee.antoine@soprasteria.com>
2019-11-22 01:12:11 +0100

    [JTA/JAE] : Ajout trame générale du programme
    [JTA/JAE] : Ajout de méthode vide à l'ordonnanceur pour garder une idée des choses à accomplir

--- src/maestro/Ordonnanceur.java
+++ src/maestro/Ordonnanceur.java
@@ -12,7 +12,36 @@ public class Ordonnanceur {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		saluer();
+		demanderCheminAAnalyser();
+		demanderOptionsAnalyse();
+		analyserChemin();
+		restituerAnalyse();
+	}
 
+	private static void restituerAnalyse() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void analyserChemin() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void demanderOptionsAnalyse() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void demanderCheminAAnalyser() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void saluer() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }

50aa748f1c99c4d192f58944b040fc09ccddfe8e
Kal El <jt.antoine@gmail.com>
2020-02-03 21:08:49 +0100

    Update IOConsoleService.java
    [JTA/JAE] : Ajout de vérification avant affichage de message

--- src/services/IOConsoleService.java
+++ src/services/IOConsoleService.java
@@ -2,6 +2,9 @@ package services;
 
 import java.util.Scanner;
 
+import org.apache.commons.lang3.StringUtils;
+
+
 /**
  * Classe servant à gérer les entrées et sorties via la console
  * @author jtantoine
@@ -11,21 +14,25 @@ public class IOConsoleService {
 	
 	/**
 	 * Méthode permettant d'afficher du texte en console
+	 * 
 	 * @param msgParam
 	 */
-	//TODO : Set a check on non blankness of variable
+	// TODO : Use a logger just in case there is a problem with the content
 	public static void displayMessageInConsole(String msgParam) {
-		System.out.println(msgParam);
+		if (StringUtils.isNotBlank(msgParam)) {
+			System.out.println(msgParam);
+		}
 	}
 	
 	/**
 	 * Méthode permettant de récupérer les entrants en provenance de la console
-	 * @return
+	 * @return ce que l'utilisateur a récupéré
 	 */
-	//TODO : Put the expected type as a parameter
+	//TODO : create utils method 
 	public static String fetchDataFromConsole() {
 		Scanner scanner = new Scanner(System.in);
 		String incomingValue = scanner.next();
+		scanner.close();
 		return incomingValue;
 	}
 

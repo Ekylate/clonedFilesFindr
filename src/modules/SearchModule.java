6c202067d38501277bd58ff030523ecb8e235f34
Ekylate <jt.antoine@gmail.com>
2020-02-24 14:44:16 +0100

    [JTA/JAE] : update SearchModule.java
    [JTA/JAE] : Renommage variable + Ajout ébauche méthode d'exploration

--- src/modules/SearchModule.java
+++ src/modules/SearchModule.java
@@ -1,8 +1,12 @@
 package modules;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.lang3.StringUtils;
 
 import BusinessObjects.SeachResultBO;
+import services.FileService;
 
 /**
  * Class used to search through directories
@@ -10,21 +14,37 @@ import BusinessObjects.SeachResultBO;
  *
  */
 public class SearchModule {
-	private String targetNode = StringUtils.EMPTY;
+	private String rootNode = StringUtils.EMPTY;
 	private SeachResultBO resultBo = new SeachResultBO();
+	private FileService fileService = new FileService();
+	private List<String> notParsedYet = new ArrayList<String>();
+	
+	//TODO : remanier méthode pour que récursivité ou boucle
+	public void explore() {
+		String targetNode = StringUtils.EMPTY;
+		
+		if(StringUtils.isNotBlank(rootNode)) {
+			if(fileService.isParamADirectory(rootNode)) {
+				// TODO : ajouter une récursivité
+				notParsedYet.add(fileService.listFilesInDirectory(rootNode));
+			}
+			//TODO : use LOGGER here in case targetNode is not a directory
+		} 
+		//TODO : use LOGGER here in case targetNode is empty
+	}
 
 	/**
 	 * @return the targetNode
 	 */
 	public String getTargetNode() {
-		return targetNode;
+		return rootNode;
 	}
 
 	/**
 	 * @param targetNode the targetNode to set
 	 */
 	public void setTargetNode(String targetNode) {
-		this.targetNode = targetNode;
+		this.rootNode = targetNode;
 	}
 
 	/**

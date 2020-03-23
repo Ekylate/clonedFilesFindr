61c7d7de16c59aee31c2398b613394331677b437
JT_SoprSter_Laptop <jean-timothee.antoine@soprasteria.com>
2020-03-23 01:15:41 +0100

    [JTA/JAE] : Ajout de méthode pour concaténation liste de chaînes de caractères vers une seule chaîne de caractères multiligne

--- src/maestro/utils/StringUtils.java
+++ src/maestro/utils/StringUtils.java
@@ -1,5 +1,7 @@
 package maestro.utils;
 
+import java.util.List;
+
 /**
  * Handmade class used because of the lack of the conventionnal class of the same name
  * @author jtantoine
@@ -7,6 +9,8 @@ package maestro.utils;
  */
 public class StringUtils {
 	public static final String EMPTY = "";
+	
+	private StringUtils() { }
 	
 	public static boolean isEmpty(String paramToCheck) {
 		return (paramToCheck != null && paramToCheck.isEmpty());
@@ -22,5 +26,25 @@ public class StringUtils {
 
 	public static boolean isNotBlank(String paramToCheck) {
 		return !isBlank(paramToCheck);
+	}
+	
+	/**
+	 * Concatenate the elements of the list in a multilined String. And the offset
+	 * will be in the beginning of each line oh the final multilined String
+	 * 
+	 * @param listParam list of the elements to concatenate
+	 * @param offset    offset to put in the beginning of each line
+	 * @return
+	 */
+	public static String concatenateStringListToSingleMultilinedString(List<String> listParam, String offset) {
+		String result = EMPTY;
+		if (listParam != null && !listParam.isEmpty()) {
+			StringBuilder strBuildr = new StringBuilder();
+			for (String elmt : listParam) {
+				strBuildr.append(offset).append(elmt).append(System.lineSeparator());
+			}
+			result.concat(strBuildr.toString());
+		}
+		return result;
 	}
 }

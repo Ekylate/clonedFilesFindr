25dd539edf0ce8aae8b2361d6fbfa373be8226ac
Kal El <jt.antoine@gmail.com>
2020-04-12 21:14:55 +0200

    Update SearchModule.java
    [JTA/JAE] : Ajout de messages de log

--- src/modules/SearchModule.java
+++ src/modules/SearchModule.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import BusinessObjects.SeachResultBO;
 import services.FileService;
@@ -18,6 +20,7 @@ public class SearchModule {
 	private SeachResultBO resultBo = new SeachResultBO();
 	private FileService fileService = new FileService();
 	private List<String> notParsedYet = new ArrayList<String>();
+	private Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 	
 	//TODO : remanier méthode pour que récursivité ou boucle
 	public void explore() {
@@ -27,10 +30,12 @@ public class SearchModule {
 			if(fileService.isParamADirectory(rootNode)) {
 				// TODO : ajouter une récursivité
 				notParsedYet.add(fileService.listFilesInDirectory(rootNode));
+			} else {
+				LOGGER.debug("Element rencontré non répertoire : {}", rootNode);
 			}
-			//TODO : use LOGGER here in case targetNode is not a directory
-		} 
-		//TODO : use LOGGER here in case targetNode is empty
+		} else {
+			LOGGER.warn("Noeud de départ vide, programme de recherche non lancé");
+		}
 	}
 
 	/**
